70139b5285505cac113961084944b2abaebadbf1
Jorge Aldao <jaldao27@gmail.com>
2019-10-26 10:52:27 +0200

    form modified

--- src/templates/elements.js
+++ src/templates/elements.js
@@ -18,7 +18,7 @@ export default class Elements extends React.Component {
                         </header>
                         <section>
                             <h4>Form</h4>
-                            <form name="contact" data-netlify="true" action="/">
+                            <form name="contact" data-netlify="true" netlify method="post" action="thank-you">
                                 <div className="row gtr-uniform gtr-50">
                                     <div className="col-6 col-12-xsmall">
                                         <input type="text" name="name" id="demo-name"  placeholder="Name" />
